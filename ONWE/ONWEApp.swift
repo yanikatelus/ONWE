15edba49f29569ab2d77a3cf90c716e4735e136b
yanikatelus <yanikatelus@gmail.com>
2022-04-21 22:08:26 -0400

    Initial Commit

---
+++ ONWE/ONWEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONWEApp.swift
+//  ONWE
+//
+//  Created by Yanika Telus on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ONWEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
